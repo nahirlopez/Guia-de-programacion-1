102840c3d8a44fc87156f583cae97cb23d8b33d0
nahirlopez <101569242+nahirlopez@users.noreply.github.com>
2022-03-30 15:51:11 -0300

    ejercicio 7 terminado

--- Assets/Ejercicio7.cs
+++ Assets/Ejercicio7.cs
@@ -4,10 +4,28 @@ using UnityEngine;
 
 public class Ejercicio7 : MonoBehaviour
 {
+    public int num1;
+    public int num2;
+    public int num3;
     // Start is called before the first frame update
     void Start()
     {
-        
+        if(num1>num2 && num1 > num3)
+        {
+            Debug.Log("El mayor número es " + num1);
+        }
+        else if (num2 > num1 && num2 > num3)
+        {
+            Debug.Log("El mayor número es " + num2);
+        }
+        else if (num3 > num2 && num3 > num1)
+        {
+            Debug.Log("El mayor número es " + num2);
+        }
+        else
+        {
+            Debug.Log("Los números son iguales");
+        }
     }
 
     // Update is called once per frame
