a46be8a88e7296df5cf06aca62d3a9e1dd7dc4ac
nahirlopez <101569242+nahirlopez@users.noreply.github.com>
2022-03-30 15:40:59 -0300

    ejercicio 8 con kuki :P

---
+++ Assets/Ejercicio8.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ejercicio8 : MonoBehaviour
+{
+    public int dia;
+    // Start is called before the first frame update
+    void Start()
+    {
+        switch (dia)
+        {
+            case 1:
+                Debug.Log("Domingo");
+                break;
+
+            case 2:
+                Debug.Log("Lunes");
+                break;
+
+            case 3:
+                Debug.Log("Martes");
+                break;
+
+            case 4:
+                Debug.Log("Miercoles");
+                break;
+
+            case 5:
+                Debug.Log("Jueves");
+                break;
+
+            case 6:
+                Debug.Log("Viernes");
+                break;
+
+            case 7:
+                Debug.Log("Sabado");
+                break;
+
+            default:
+                Debug.Log("El día ingresado no es válido");
+                break;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
